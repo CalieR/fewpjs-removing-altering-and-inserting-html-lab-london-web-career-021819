82c479c050d095899eed17a3ab56025cf619511f
CalieR <calielrushton@gmail.com>
2019-03-31 19:04:08 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let element = document.querySelector("main");
+element.remove();
+
+let newHeader = document.createElement("h1");
+document.body.appendChild(newHeader);
+newHeader.id = "victory";
+newHeader.innerText = "Calie is the champion";
